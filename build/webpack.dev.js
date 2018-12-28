e55c22b04f3077b1969e65dd694690a8132294e1
xianjixin <274804853@qq.com>
2018-12-28 18:03:53 +0800

    [chore] webpack配置代理转发

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -3,23 +3,35 @@ const webpackMerge = require("webpack-merge");
 const webpack = require("webpack")
 const path = require("path")
 module.exports = webpackMerge(commonConfig, {
-    mode: "development",
-    output: {
-        path:  path.resolve(__dirname, "../dist"),
-        publicPath: "/",
-        filename: "js/[name].js",
-    },
-    devServer: {
-        // inline:true,
-        open: true,
-        historyApiFallback: true,
-        hot: true,
-        port: 8000,
-        stats: 'minimal',
-        compress: true,
-        contentBase: path.resolve(__dirname, "../dist"),
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-    ]
+  mode: "development",
+  // target: "web",
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: "/",
+    filename: "js/[name].js"
+  },
+  devServer: {
+    // inline:true,
+    open: true,
+    historyApiFallback: true,
+    hot: true,
+    port: 8000,
+    stats: 'minimal',
+    compress: true,
+    openPage: "html/index.html",
+    contentBase: path.resolve(__dirname, "../dist"),
+    proxy: {
+      '/api/*': {
+        target: 'http://cloud.xiongmaozhanggui.com/',
+        pathRewrite: {
+          '/api': ''
+        },
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ]
 })
